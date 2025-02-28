076d1d28128c58ca8763e44649e212c306229a9c
Bryz Quartz <quartzermia@gmail.com>
2025-02-28 11:50:07 -0800

    fixed postscript error

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Fix SSH key permissions
-chmod 600 /home/arcanine/.ssh/id_rsa
-chmod 644 /home/arcanine/.ssh/id_rsa.pub
-
 # Add SSH key
 eval "$(ssh-agent -s)"
 ssh-add /home/arcanine/.ssh/id_rsa
 
 # Set workspace ownership
 sudo chown -R arcanine:arcanine /workspace
-sudo chmod -R 775 /workspace &&
+sudo chmod -R 775 /workspace
